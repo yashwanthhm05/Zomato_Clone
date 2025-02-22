1f072d5ff4f3d9b3da51c582b840bf5735840c42
Yashwanth H M <yashwanthhm05@gmail.com>
2025-02-22 14:58:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { AllRoutes } from './Components/AllRoutes/AllRoutes';
+import { Footer } from './Components/Footer/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
